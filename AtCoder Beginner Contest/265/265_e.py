aa7d5f13679259d9cbcdbe3e7b6c97cf77a44437
Tsora1216 <so12ra16@icloud.com>
2022-08-21 22:37:36 +0900

    main

---
+++ AtCoder Beginner Contest/265/265_e.py
@@ -0,0 +1,11 @@
+N,M=list(map(int,input().split()))
+A,B,C,D,E,F=list(map(int,input().split()))
+Slist=[]
+for i in range(M):
+    temp_list=list(map(int,input().split()))
+    Slist.append(temp_list)
+
+#print(N,M)
+#print(A,B,C,D,E,F)
+#print(Slist)
+
